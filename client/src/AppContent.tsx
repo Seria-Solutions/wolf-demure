c6ba643f2dd2c696f7dfdf1b0a97377f8dfa4661
Joel Kallstrom <joelkallstrom@outlook.com>
2025-01-22 23:51:13 +0000

    architecture - separate landing page, set up routes page

---
+++ client/src/AppContent.tsx
@@ -0,0 +1,7 @@
+import LandingPage from "./LandingPage";
+
+// We'll be using the `AppContent` component to set up routes for our application.
+
+export default function AppContent() {
+  return <LandingPage />;
+}
